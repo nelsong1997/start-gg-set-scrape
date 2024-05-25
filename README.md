58e18147bdd3f044fd54303d9f12b4e151ca685c
Gabe Nelson <nelsong1997@gmail.com>
2024-05-24 23:37:15 -0400

    rewrite using HTML DOM functions, works both with and without seeds

---
+++ README.md
@@ -0,0 +1,11 @@
+## Start.gg set data web scraping tool
+
+Note: I only tested this with google chrome. The auto copy functionality might not work with other browsers, but with some small modifications this script can work with those too.
+
+1. navigate to the bracket
+2. hit f12
+3. paste javascript into the console
+4. hit enter (tab delimited data will be copied to your clipboard)
+5. paste into spreadsheet
+
+Output columns: Player 1 tag, player 1 score, player 2 tag, player 2 score. The output includes a row for every set played.
--- scrape.js
+++ scrape.js
@@ -1,22 +1,25 @@
-// *in the admin section of start.gg,* navigate to the bracket, hit f12 and paste this into the console and hit enter
-// within 2 seconds, click in the background of the bracket page
-// if you clicked in time, a message will appear in the console saying "copied"
-// otherwise you'll get an error
-// once it's copied you can paste directly into a spreadsheet
-
-function getElementGreatNthGrandchild(element, n) {
-    if (n===0) return element
-    n--
-    return getElementGreatNthGrandchild(element.children.item(0), n)
-}
-
 function getPlayerTag(block) {
-    let player1TagAndSeed = getElementGreatNthGrandchild(block, 6).textContent
-    return player1TagAndSeed.split(' ').slice(1).join(' ')
+    playerTagBlock = block.getElementsByClassName('match-player-name-container')[0]
+    if (playerTagBlock.children.length > 0) { // includes seed
+        return playerTagBlock.textContent.split(' ').slice(1).join(' ')
+    } else { // just the tag 
+        return playerTagBlock.textContent
+    }
 }
 
 function getPlayerScore(block) {
-    return getElementGreatNthGrandchild(block, 2).children.item(1).children.item(0).textContent
+    return block.getElementsByClassName('match-player-stocks')[0].textContent
+}
+
+// https://stackoverflow.com/questions/63033012/copy-the-text-to-the-clipboard-without-using-any-input
+function copyTextToClipboard(text) {
+    let tempTextarea = document.createElement('textarea')
+    tempTextarea.value = text
+    document.body.appendChild(tempTextarea)
+    tempTextarea.select()
+    
+    document.execCommand("copy")
+    document.body.removeChild(tempTextarea)
 }
 
 let returnString = ''
@@ -24,15 +27,10 @@ let returnString = ''
 let sets = document.getElementsByClassName('match-affix-wrapper')
 
 for (let set of sets) {
-    let player1Block = set.children.item(0)
-    let player2Block = set.children.item(2)
+    let player1Block = set.getElementsByClassName('match-section-top')[0]
+    let player2Block = set.getElementsByClassName('match-section-bottom')[0]
     returnString += getPlayerTag(player1Block) + '\t' + getPlayerScore(player1Block) + '\t'
         + getPlayerTag(player2Block) + '\t' + getPlayerScore(player2Block) + '\n'
 }
 
-//console.log(returnString)
-
-setTimeout(() => {
-    navigator.clipboard.writeText(returnString)
-    console.log('copied')
-}, 2000)
+copyTextToClipboard(returnString)
